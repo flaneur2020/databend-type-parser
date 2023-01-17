2dd751dda7c84f9b564dac7225d92976cb83c008
Li Yazhou <me.ssword@gmail.com>
2023-01-17 19:17:42 +0800

    wrap

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-enum TypeName {
+enum TypeKind {
     Int8 = "Int8",
     Int16 = "Int16",
     Int32 = "Int32",
@@ -9,22 +9,24 @@ enum TypeName {
     UInt64 = "UInt64",
     Float32 = "Float32",
     Float64 = "Float64",
+    Variant = "Variant",
+    JSON = "JSON",
     String = "String",
     Array = "Array",
     Nullable = "Nullable",
 }
 
-function parseTypeName(t: string): TypeName | undefined {
-    return (<any>TypeName)[t];
+function parseTypeKind(t: string): TypeKind | undefined {
+    return (<any>TypeKind)[t];
 }
 
-function isNumericType(t: TypeName): boolean {
-    let typeString: string = TypeName[t];
+function isNumericType(t: TypeKind): boolean {
+    let typeString: string = TypeKind[t];
     return typeString.includes("Int") || typeString.includes("Float") || typeString.includes("decimal");
 }
 
-function isComposedType(t: TypeName): boolean {
-    return [TypeName.Array, TypeName.Nullable].includes(t)
+function isComposedType(t: TypeKind): boolean {
+    return [TypeKind.Array, TypeKind.Nullable].includes(t)
 }
 
 type ParseError = {
@@ -37,7 +39,7 @@ function isParseError(obj: any): obj is ParseError {
 }
 
 type ColumnType = {
-  type: TypeName;
+  type: TypeKind;
   child?: ColumnType;
   isNumeric: boolean;
 };
@@ -47,17 +49,17 @@ function parseColumnTypeByTokens(tokens: Array<string>): ColumnType | ParseError
         return { error: "eof" }
     }
 
-    let typeName = parseTypeName(tokens[0])
-    if (!typeName) {
+    let typeKind = parseTypeKind(tokens[0])
+    if (!typeKind) {
         return { error: "unknown_type", message: `unknown type ${tokens[0]}` }
     }
 
     let columnType: ColumnType = {
-        type: typeName,
-        isNumeric: isNumericType(typeName),
+        type: typeKind,
+        isNumeric: isNumericType(typeKind),
     }
 
-    if (isComposedType(typeName)) {
+    if (isComposedType(typeKind)) {
         let nextTokens = tokens.slice(1, tokens.length);
         console.log(`tokens: ${tokens}, next Tokens: ${nextTokens}`);
         let r = parseColumnTypeByTokens(nextTokens)
@@ -78,7 +80,8 @@ function parseColumnType(t: string): ColumnType | ParseError {
     return parseColumnTypeByTokens(tokens)
 }
 
-console.log(parseTypeName("Nullable"));
-console.log(parseTypeName("blah"));
-console.log(isNumericType(TypeName.String));
+console.log(parseTypeKind("Nullable"));
+console.log(parseTypeKind("blah"));
+console.log(isNumericType(TypeKind.String));
 console.log(parseColumnType("Nullable(Array(UInt32))"));
+console.log(parseColumnType("Array(UInt32)"));
